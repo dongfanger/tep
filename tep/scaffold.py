d09dbc0f175f06ad12df71382ba85aec2597c44b
dongfanger <dongfang@class100.com>
2020-08-13 12:59:22 +0800

    _allure_temp

--- tep/scaffold.py
+++ tep/scaffold.py
@@ -54,27 +54,45 @@ def create_scaffold(project_name):
     conftest_content = """import os
 import shutil
 
+import allure_commons
+from allure_commons.logger import AllureFileLogger
+from allure_pytest.listener import AllureListener
+from allure_pytest.plugin import cleanup_factory
 from tep.funcs import current_date
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 
 reports_html = os.path.join(project_dir, 'reports', 'report-' + current_date())
 
+_allure_temp = '.allure-temp-auto-del'
+
 
 def pytest_addoption(parser):
     parser.addoption(
-        '--tepreports',
+        '--tep-reports',
         action='store_const',
         const=True,
         help='Create tep reports and open automatically.'
     )
 
 
+def pytest_configure(config):
+    if config.getoption('--tep-reports'):
+        test_listener = AllureListener(config)
+        config.pluginmanager.register(test_listener)
+        allure_commons.plugin_manager.register(test_listener)
+        config.add_cleanup(cleanup_factory(test_listener))
+
+        clean = config.option.clean_alluredir
+        file_logger = AllureFileLogger(_allure_temp, clean)
+        allure_commons.plugin_manager.register(file_logger)
+        config.add_cleanup(cleanup_factory(file_logger))
+
+
 def pytest_sessionfinish(session):
-    allure_report_dir = session.config.getoption('allure_report_dir')
-    if allure_report_dir and session.config.getoption('--tepreports'):
-        os.system(f"allure generate {allure_report_dir} -o {reports_html}  --clean")
-        shutil.rmtree(allure_report_dir)
+    if session.config.getoption('--tep-reports'):
+        os.system(f"allure generate {_allure_temp} -o {reports_html}  --clean")
+        shutil.rmtree(_allure_temp)
         os.system(f"allure open {reports_html}")
 """
 
