9dfd431d2e6ac8f0485bc9d55bfda9793b583f87
dongfanger <dongfang@class100.com>
2020-08-14 17:57:17 +0800

    path

--- tep/path.py
+++ tep/path.py
@@ -1,13 +1,6 @@
-#!/usr/bin/python
-# encoding=utf-8
-
-"""
-@Author  :  Don
-@Date    :  8/14/2020 5:46 PM
-@Desc    :  
-"""
-
-
 class Path:
-    reports_html = ''
-    allure_temp = '.allure-temp-auto-del'
+    project_dir = ''
+
+    @staticmethod
+    def set_project_dir(dir_):
+        Path.project_dir = dir_
