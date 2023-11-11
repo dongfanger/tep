3e4b035270ae765d37e3c29dcfc29795ab50cabd
wanggang424 <wanggang424@jd.com>
2023-11-11 14:35:40 +0800

    Fix assert

--- tests/demo/case/test_assert.py
+++ tests/demo/case/test_assert.py
@@ -14,14 +14,14 @@ def test_assert_less_than():
     assert 1 < 2
 
 
-def test_assert_less_or_equals():
-    assert 2 >= 1
-    assert 2 >= 2
-
-
 def test_assert_greater_or_equals():
+    assert 2 >= 1
+    assert 1 >= 1
+
+
+def test_assert_less_or_equals():
     assert 1 <= 2
-    assert 1 <= 1
+    assert 2 <= 2
 
 
 def test_assert_length_equal():
@@ -54,15 +54,31 @@ def test_assert_startswith():
     assert "dongfanger".startswith("don")
 
 
+def test_assert_endswith():
+    assert "dongfanger".endswith("er")
+
+
 def test_assert_regex_match():
     import re
     assert re.findall(r"don.*er", "dongfanger")
 
 
 def test_assert_contains():
-    assert "fang" in "dongfanger"
+    class Res:
+        code = 200
+        msg = "success"
+
+    assert "success" in Res.msg
     assert 2 in [2, 3]
     assert "x" in {"x": "y"}.keys()
+
+
+def test_assert_contained_by():
+    class Res:
+        code = 200
+        msg = "success"
+
+    assert Res.code in [0, 200]
 
 
 def test_assert_type_match():
