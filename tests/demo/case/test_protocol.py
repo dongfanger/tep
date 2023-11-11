f79c641d5cdbc4703d6cda7077deba74d0947e86
wanggang424 <wanggang424@jd.com>
2023-11-12 00:13:03 +0800

    Add test protocol

--- tests/demo/case/test_protocol.py
+++ tests/demo/case/test_protocol.py
@@ -1,3 +1,4 @@
+from httpx import Response
 from loguru import logger
 
 from tep.libraries.TepResponse import TepResponse
@@ -24,13 +25,13 @@ def test(HTTPRequestKeyword):
     logger.info("HTTP/2 get")
     body = {"foo1": "foo1", "foo2": "foo2"}
     ro = HTTPRequestKeyword("get", url=ctx["baseUrl"] + "/get", headers={"User-Agent": "tep"}, params=body, http2=True)
-    response: TepResponse = ro.response
+    response: Response = ro.response
     assert response.status_code == 200
     assert len(body["foo1"]) == 4
 
     logger.info("HTTP/2 post")
     body = {"foo1": "foo1", "foo2": "foo2"}
     ro = HTTPRequestKeyword("post", url=ctx["baseUrl"] + "/post", headers={"User-Agent": "tep"}, params=body, http2=True)
-    response: TepResponse = ro.response
+    response: Response = ro.response
     assert response.status_code == 200
     assert len(body["foo1"]) == 4
