5a053a86178b715e8c5a32e37252a5fb4b4e549b
wanggang424 <wanggang424@jd.com>
2023-11-28 18:41:46 +0800

    ADD retry

---
+++ tests/demo/case/retry/test_retry_exception.py
@@ -0,0 +1,14 @@
+from loguru import logger
+from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type
+
+
+@retry(
+    stop=stop_after_attempt(3),
+    wait=wait_fixed(2),
+    retry=retry_if_exception_type(TypeError),
+    before=lambda _: logger.info("Retrying..."),
+    after=lambda _: logger.info("Retry completed.")
+)
+def test_exception(HTTPRequestKeyword):
+    ro = HTTPRequestKeyword("get", url="http://127.0.0.1:5000/retry/200")
+    raise TypeError
