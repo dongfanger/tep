1e5eb1b51c20d3ed44a1e7b901fb167a4204a022
wanggang424 <wanggang424@jd.com>
2023-12-05 09:14:34 +0800

    UPDATE test_flow.py && README.md

--- tests/demo/case/test_flow.py
+++ tests/demo/case/test_flow.py
@@ -1,31 +1,48 @@
-def test(HTTPRequestKeyword, JSONKeyword, login):
+def test(HTTPRequestKeyword, JSONKeyword, VarKeyword, login):
     headers = login()
-    var = {"domain": "http://127.0.0.1:5000", "headers": headers}
+    var = VarKeyword({
+        "domain": "http://127.0.0.1:5000",
+        "headers": headers
+    })
 
     url = var["domain"] + "/searchSku" + "?skuName=book"
     response = HTTPRequestKeyword("get", url=url, headers=var["headers"])
     assert response.status_code < 400
-    sku_id = response.jsonpath("$.skuId")
-    sku_price = response.jsonpath("$.price")
+    var["skuId"] = response.jsonpath("$.skuId")
+    var["skuPrice"] = response.jsonpath("$.price")
 
     url = var["domain"] + "/addCart"
-    body = r"""{"skuId":1,"skuNum":2}"""
-    body = JSONKeyword(body, {"$.skuId": sku_id})
+    body = JSONKeyword(r"""
+{
+    "skuId":"${skuId}",
+    "skuNum":2
+}
+""")
     response = HTTPRequestKeyword("post", url=url, headers=var["headers"], json=body)
     assert response.status_code < 400
-    sku_num = response.jsonpath("$.skuNum")
-    total_price = response.jsonpath("$.totalPrice")
+    var["skuNum"] = response.jsonpath("$.skuNum")
+    var["totalPrice"] = response.jsonpath("$.totalPrice")
 
     url = var["domain"] + "/order"
-    body = r"""{"skuId":1,"price":2,"skuNum":3,"totalPrice":4}"""
-    body = JSONKeyword(body, {"$.skuId": sku_id, "$.price": sku_price, "$.skuNum": sku_num, "$.totalPrice": total_price})
+    body = JSONKeyword(r"""
+{
+    "skuId":"${skuId}",
+    "price":${skuPrice},
+    "skuNum":${skuNum},
+    "totalPrice":${totalPrice}
+}
+""")
     response = HTTPRequestKeyword("post", url=url, headers=var["headers"], json=body)
     assert response.status_code < 400
-    order_id = response.jsonpath("$.orderId")
+    var["orderId"] = response.jsonpath("$.orderId")
 
     url = var["domain"] + "/pay"
-    body = r"""{"orderId":1,"payAmount":"0.2"}"""
-    body = JSONKeyword(body, {"$.orderId": order_id})
+    body = JSONKeyword(r"""
+{
+    "orderId":"${orderId}",
+    "payAmount":"0.2"
+}
+""")
     response = HTTPRequestKeyword("post", url=url, headers=var["headers"], json=body)
     assert response.status_code < 400
     assert response.jsonpath("$.success") == "true"
