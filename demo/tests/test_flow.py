01ff587823a96f1af25fca3ae21fcf358b0bff79
wanggang424 <wanggang424@jd.com>
2025-01-13 09:43:18 +0800

    更新示例代码

--- demo/tests/test_flow.py
+++ demo/tests/test_flow.py
@@ -7,35 +7,13 @@ from tep import v
 from data.global_data import GlobalData
 
 
-class Data:
+def test():
     v({
         "domain": GlobalData.domain,
         "skuNum": 2,
         "payAmount": 0.2
     })
-    body_add_cart = """
-{
-    "skuId": "${skuId}",
-    "skuNum":${skuNum}
-}
-"""
-    body_order = """
-{
-    "skuId": "${skuId}",
-    "price":${skuPrice},
-    "skuNum":${skuNum},
-    "totalPrice":${totalPrice}
-}
-"""
-    body_pay = """
-{
-    "orderId": "${orderId}",
-    "payAmount":${payAmount}
-}
-"""
 
-
-def test():
     step("查询商品", step_search_sku)
     step("添加购物车", step_add_cart)
     step("下单", step_order)
@@ -52,8 +30,13 @@ def step_search_sku():
 
 def step_add_cart():
     url = v("${domain}/addCart")
-    body = v(Data.body_add_cart)
-    response = request("post", url=url, headers=GlobalData.headers, json=json.loads(body))
+    body = """
+{
+    "skuId": "${skuId}",
+    "skuNum":${skuNum}
+}
+"""
+    response = request("post", url=url, headers=GlobalData.headers, json=json.loads(v(body)))
     assert response.status_code < 400
     v("skuNum", response.jsonpath("$.skuNum")[0])
     v("totalPrice", response.jsonpath("$.totalPrice")[0])
@@ -61,15 +44,27 @@ def step_add_cart():
 
 def step_order():
     url = v("${domain}/order")
-    body = v(Data.body_order)
-    response = request("post", url=url, headers=GlobalData.headers, json=json.loads(body))
+    body = """
+{
+    "skuId": "${skuId}",
+    "price":${skuPrice},
+    "skuNum":${skuNum},
+    "totalPrice":${totalPrice}
+}
+"""
+    response = request("post", url=url, headers=GlobalData.headers, json=json.loads(v(body)))
     assert response.status_code < 400
     v("orderId", response.jsonpath("$.orderId")[0])
 
 
 def step_pay():
     url = v("${domain}/pay")
-    body = v(Data.body_pay)
-    response = request("post", url=url, headers=GlobalData.headers, json=json.loads(body))
+    body = """
+{
+    "orderId": "${orderId}",
+    "payAmount":${payAmount}
+}
+"""
+    response = request("post", url=url, headers=GlobalData.headers, json=json.loads(v(body)))
     assert response.status_code < 400
     assert response.jsonpath("$.success")[0] == "true"
